9dbfb6d686e4056d502f810c3a6e4a6aae237d02
Nguyễn Hải <you@example.com>
2020-11-12 16:49:46 +0700

    Up date 11/12/2020

---
+++ app/src/main/java/com/example/appdongian/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.appdongian;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
